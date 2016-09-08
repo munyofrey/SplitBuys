c3d6c6ac13186be83197dd34729d3a2b758da066
Munyo Frey <munyofrey@gmail.com>
2016-09-08 11:46:44 -0700

    added better splash page

--- frontend/components/guest_component.jsx
+++ frontend/components/guest_component.jsx
@@ -14,19 +14,19 @@ class GuestComponent extends React.Component{
   }
 
   render(currentUser){
-    if (this.props.path.length > 1){return <div></div>}else{    
+    if (this.props.path.length > 1){
+      return <div></div>
+    }else{
       return(
-        <div className='splash-holder'>
-          <div className='header-block-fix'/>
-          <div className='splash-holder-center'>
-            <div className='tagline-holder'>
+          <div className='entry-image-holder'>
+
+            <div className='entrance-holder'>
               <p className='tagline'>Whether it be a trip to San Francisco or your electricity bill splitting bills with friends just got easier</p>
-            </div>
 
             {this.props.loggedin ? '' :<div onClick={this.loginuser} className='guest-login button'>Guest login</div>}
           </div>
-          <div className='entry-image-holder'></div>
-        </div>
+          </div>
+
       )
     }
   }
